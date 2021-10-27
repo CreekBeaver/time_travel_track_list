5c3bb5ad4009cf9ea930c9fcaf419adf901b1379
Kyle Creek <Kyle.A.Creek@gmail.com>
2021-10-26 17:37:27 -0700

    Updating some of the HTML and Service Information

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 url = 'http://localhost:9115/'
-data = {'name' : 'amoxicillin'}
+data = {'name': 'advil'}
 jsonData = json.dumps(data)
 r = requests.post(url=url, json=jsonData)
 
