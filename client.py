6a834c59f9fc60c5ca2674adc56e9d2e4894a4ca
Kyle Creek <Kyle.A.Creek@gmail.com>
2021-11-01 13:08:07 -0700

    Added requirements file strictly for the server, This should work, it worked on heroku

--- client.py
+++ client.py
@@ -3,9 +3,9 @@ import json
 
 #url = 'http://localhost:9115/'
 url = 'https://young-shore-84821.herokuapp.com/'
-data = {'name': 'advil'}
+data = {'name': 'test'}
 jsonData = json.dumps(data)
 r = requests.post(url=url, json=jsonData)
 
-d = r.json()
-print(d)
+#d = r.json()
+print(r)
