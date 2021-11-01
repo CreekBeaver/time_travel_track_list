bcbc56bc8c69c79eee343b76bcf0415347367b79
Kyle Creek <Kyle.A.Creek@gmail.com>
2021-11-01 12:54:48 -0700

    Got this working in a practice, now want to get onto flip servers

--- client.py
+++ client.py
@@ -2,10 +2,10 @@ import requests
 import json
 
 #url = 'http://localhost:9115/'
-url = 'https://aqueous-savannah-87139.herokuapp.com/'
+url = 'https://young-shore-84821.herokuapp.com/'
 data = {'name': 'advil'}
 jsonData = json.dumps(data)
 r = requests.post(url=url, json=jsonData)
 
-#d = r.json()
-print(r)
+d = r.json()
+print(d)
