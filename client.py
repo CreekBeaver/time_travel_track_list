cba04f20314f70ba8945ed0a593c15b29ff02892
Kyle Creek <Kyle.A.Creek@gmail.com>
2021-11-01 11:45:05 -0700

    Adding Procfile

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
 import requests
 import json
 
-url = 'http://localhost:9115/'
+#url = 'http://localhost:9115/'
+url = 'https://aqueous-savannah-87139.herokuapp.com/'
 data = {'name': 'advil'}
 jsonData = json.dumps(data)
 r = requests.post(url=url, json=jsonData)
 
-d = r.json()
-print(d)
+#d = r.json()
+print(r)
